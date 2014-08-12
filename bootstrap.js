95396c1fbaa8dd9800b85513dcce14799ecfa503
steven-gardiner <steven_gardiner@yahoo.com>
2014-08-12 16:13:38 -0400

    towards noop

--- bootstrap.js
+++ bootstrap.js
@@ -12,10 +12,7 @@ boot.onOpenWindow = function(aWindow) {
       // If this is a browser window then setup its UI
     if (domWindow.document.documentElement.getAttribute("windowtype") == "navigator:browser") {
       var callback = function() {
-        if (boot.exports) {
-          if (boot.exports.addWindow) {
-            boot.exports.addWindow(domWindow);
-          }
+        if (boot.modules) {
           boot.injectModules(domWindow);
           boot.emit(domWindow.document, "insert-mozilla-ui");    
         } else {
@@ -32,15 +29,11 @@ boot.onCloseWindow = function(aWindow) { };
 boot.onWindowTitleChange = function() { };
 
 function startup(data,reason) {
-
   let windows = Services.wm.getEnumerator("navigator:browser");
   while (windows.hasMoreElements()) {
     let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
     var callback = function() {
-      if (boot.exports) {
-        if (boot.exports.addWindow) {
-          boot.exports.addWindow(domWindow);
-        }
+      if (boot.modules) {
         boot.injectModules(domWindow);
         boot.emit(domWindow.document, "insert-mozilla-ui");
       } else {
@@ -54,24 +47,53 @@ function startup(data,reason) {
 
   AddonManager.getAddonByID(data.id, function(addon) {
       
-    var jsmurl = addon.optionsURL.split(/\//).slice(0,3).concat(["content",
-                                                                 "jsm",
-                                                                 "main.jsm"]).join('/');
-    var jsurl = addon.optionsURL.split(/\//).slice(0,3).concat(["content",
-                                                                "js",
-                                                                "main.js"]).join('/');
+    var urlparts = addon.optionsURL.split(/\//);
+
+    var extname = urlparts[2];
+
+    var extprefix = urlparts.slice(0,3);
+    var jsurl = extprefix.concat(["content",
+                                  "js",
+                                  "main.js"]).join('/');
+
+    var ioService = Components.classes["@mozilla.org/network/io-service;1"]
+      .getService(Components.interfaces.nsIIOService);
+    var styleService = Components.classes["@mozilla.org/content/style-sheet-service;1"]
+      .getService(Components.interfaces.nsIStyleSheetService);
 
     boot.modules = [];
     boot.windows = [];
     boot.emit = function(doc, eventType) {
       var evt = doc.createEvent("CustomEvent");
       evt.initCustomEvent(eventType, true, false, {});
-      doc.dispatchEvent(evt);           
+      doc.dispatchEvent(evt);    
     };
     boot.require = function(module) {
       boot.modules.push(module);
     };
+    boot.injectStylesheet = function(loc, handler) {
+      if (! handler) { 
+        handler = function(err) { 
+          Services.console.logStringMessage(err);           
+        }; 
+      }
+      try {
+        var uristr = extprefix.concat(["skin",loc]).join("/");
+        var uri = ioService.newURI(uristr, null, null);
+        if (! styleService.sheetRegistered(uri, styleService.AGENT_SHEET)) {
+          styleService.loadAndRegisterSheet(uri, styleService.AGENT_SHEET);
+        }      
+      } catch (err) {
+        handler(err);
+      }
+    };
+    boot.handleStylesheet = function(event,detail) {
+      detail = detail || event.detail || (event.originalEvent && event.originalEvent.detail);
+      boot.injectStylesheet(detail.stylesheet);
+    };
     boot.injectModules = function(window) {
+      window.document.addEventListener('register-stylesheet', boot.handleStylesheet, false);
+      window.env = boot.env;
       boot.modules.forEach(function(module) {
         boot.inject(module, window);
       });
@@ -93,29 +115,29 @@ function startup(data,reason) {
     boot.cleanup = function() {
       boot.windows.forEach(function(window) {
         boot.emit(window.document, "cleanup-mozilla-ui");          
+        window.document.removeEventListener('register-stylesheet', boot.handleStylesheet, false);
       });
+      boot.windows = [];
+      delete boot.modules;
     };
+    boot.env = {};
+    boot.env.extname = extname;
 
     try {
       Services.scriptloader.loadSubScript(jsurl, boot, "UTF-8");
     } catch (ee) {
       Services.console.logStringMessage(ee); 
     }
-    Components.utils.import(jsmurl, boot);
-    if (boot.exports.init) {
-      boot.exports.init();
-    }
+
   });
 
 }
 
 function shutdown(data, reason) {
-  if (boot.exports.cleanup) {
-    boot.exports.cleanup();
-  }
   boot.cleanup();
 
   Services.wm.removeListener(boot);
+  boot = null;
 }
 
 function install(data, reason) {
